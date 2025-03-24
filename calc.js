05a961e1aac17cf884f1a44baba49c6f12ea924e
aline-001 <148322512+aline-001@users.noreply.github.com>
2025-03-24 10:24:54 +0200

    this is simple calculator that perform basic operators(addition,substraction,multiplication and division)

---
+++ calc.js
@@ -0,0 +1,11 @@
+let x=2;
+let y=4;
+let sum = x+y;
+let difference=x-y;
+let product=x*y;
+let quotient=x/y;
+
+console.log("The sum of " + x +"and " + y + " is " +sum);
+console.log("The difference btn " + x + "and " + y + " is " +difference);
+console.log("The product of " + x + "and " + y + " is " +product);
+console.log("The quotient of " + x + "and " + y + " is " +quotient);
